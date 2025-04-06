0bb80a373380f7a556440bb545359797524836c5
OsvaExe <osvaldo.hernandezj@gmail.com>
2025-04-06 13:25:45 -0600

    Update parcial2.py
    Se agrego la funcion de los NA

--- parcial2.py
+++ parcial2.py
@@ -2,9 +2,8 @@
 #esto es para hacer un commit
 # Proyector-Parcial1
 #Vamos a desarollar el proyecto de python del primer parcial
-#import sys
-#import os4
-#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(_file_), '..')))
+import sys 
+import os
 ## esto es un comentario para ver como se agrega al programa principal
 def finalizar ():
     print("hasta luego")
@@ -37,11 +36,56 @@ def inicio():
             case 2:
                 print ("tercer parcial")
             case 3:
-                print ("faltas")
+                print ("Faltas")
             case 4:
                 print("NA's")
+                while True:
+                    try:
+                        s = int(input("Ingresa tu semestre actual: "))
+                        na = int(input("Ingresa el número de NA's que tienes acumulado: "))
+
+                        if s < 1 or s > 8:
+                            print("El semestre debe ser mayor a 0 y menor que 9.")
+                            continue
+                        if na < 0:
+                            print("El número de NA no debe ser menor que 0.")
+                            continue
+                        break
+                    except ValueError:
+                        print("El valor que tecleaste no es válido, debe ser un numero ENTERO mayor o igual a 0 segun sea el caso. Intente de nuevo.")
+
+                if s <= 5:
+                    def na5(na):
+                        if na >= 8:
+                            print("Estás en baja definitiva")
+                        elif 5 <= na <= 7:
+                            print("Estás en riesgo")
+                        elif 0 <= na <= 4:
+                            print("Todo bien")
+                    na5(na)
+                else:
+                    def na7(na):
+                        if na >= 11:
+                            print("Estás en baja definitiva")
+                        elif 9 <= na <= 10:
+                            print("Estás en riesgo")
+                        elif 0 <= na <= 8:
+                            print("Todo bien")
+                    na7(na)
+                    match s:
+                        case 6:
+                            if na >= 10:
+                                print("Estás en baja definitiva")
+                            elif 7 <= na <= 9:
+                                print("Estás en riesgo")
+                            elif 0 <= na <= 6:
+                                print("Todo bien")
+                        case 7:
+                            na7(na)
+                        case _:
+                            na7(na)
             case 5:
-                #print("final")
+                print("final")
                 terminar=finalizar()
     #   Aqui vamos a iniciar el programa
     
